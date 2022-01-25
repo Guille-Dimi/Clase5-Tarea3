f26182dc4e55b89c0617342ce277bc2a1991be46
Guille-Dimi <guillecalvo81@gmail.com>
2022-01-25 12:45:54 -0300

    agregado scrip para calcular y visualizar el tiempo
    Se agrego un scrip para calcular la suma de cinco videos y el html para visualizarlo en la web

--- tarea3.js
+++ tarea3.js
@@ -5,3 +5,36 @@
 // cada dato.
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
+
+
+let segundosTotal = 0
+let minutosTotal = 0
+let horasTotal = 0
+
+document.querySelector('#calcular').onclick = function(){
+    segundosTotal += Number(document.querySelector('#segundos-video1').value) +
+    Number(document.querySelector('#segundos-video2').value) +
+    Number(document.querySelector('#segundos-video3').value) +
+    Number(document.querySelector('#segundos-video4').value) +
+    Number(document.querySelector('#segundos-video5').value)
+    minutosTotal += Number(document.querySelector('#minutos-video1').value) +
+    Number(document.querySelector('#minutos-video2').value) +
+    Number(document.querySelector('#minutos-video3').value) +
+    Number(document.querySelector('#minutos-video4').value) +
+    Number(document.querySelector('#minutos-video5').value)
+    horasTotal += Number(document.querySelector('#horas-video1').value) +
+    Number(document.querySelector('#horas-video2').value) +
+    Number(document.querySelector('#horas-video3').value) +
+    Number(document.querySelector('#horas-video4').value) +
+    Number(document.querySelector('#horas-video5').value)
+
+    const resultadoSeg = segundosTotal % 60
+    const resultadoMin = Math.floor(segundosTotal / 60) + (minutosTotal % 60)
+    const resultadoHora = horasTotal + Math.floor(minutosTotal / 60)
+
+    document.querySelector('#resultado').innerText = `${ resultadoHora}:${resultadoMin}:${resultadoSeg}`
+
+    return false
+}
+
+//id="calcular-tiempo"
